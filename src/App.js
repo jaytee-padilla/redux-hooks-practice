b41b5c58d283735a83d699b8794cecc2d0532203
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-11-05 16:41:18 -0800

    Added actions to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+
+// redux actions
+import {increment, decrement} from './actions';
 
 function App() {
+	const counterReducer = useSelector(state => state.counterReducer);
+	const isLogged = useSelector(state => state.isLogged);
+	const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Yuh</h1>
+      <h1>Counter: {counterReducer}</h1>
+			<button onClick={() => dispatch(increment(5))}>+</button>
+			<button onClick={() => dispatch(decrement())}>-</button>
+
+			{isLogged ?
+			<h3>Valuable info that only a logged in user should see</h3> :
+			<p>User Is Not Logged In</p>}
     </div>
   );
 }
